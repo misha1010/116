a5bce6891aec12ff928c01d24641de15e8436eb4
amakarenko96 <60483304+amakarenko96@users.noreply.github.com>
2020-02-03 23:32:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static d main(String[] args) {
+        int i;
+        for (i=0; i<2; i++) {
 	System.out.print ("Hello Yevhenii");
+        i--;
+        }
     }
 }
