3c4947e2421ab105790d8dc84e60358397d12c52
AlexAdamantine <mralexshark@gmail.com>
2020-02-03 23:17:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static d main(String[] args) {
 	System.out.print ("Hello Yevhenii");
     }
 }
